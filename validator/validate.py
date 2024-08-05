ee2fa476cbc21de69c0367ea408f0ce3d9e0b1db
Joshua Selbo <jselbo@meta.com>
2024-08-05 09:22:00 -0500

    Set PROBLEM_NUMBER env for more flexibility for runners

--- validator/validate.py
+++ validator/validate.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 import argparse
+import os
 import sys
 import p1
 import p2
 
+PROBLEM_NUMBER_ENV = "PROBLEM_NUMBER"
+
 # python3 validator/validate.py p1 --program_exec don/p1/runner.sh
 # python3 validator/validate.py p2 --program_exec don/p2/runner.sh
+#
+# Runners can also read the PROBLEM_NUMBER environment variable (e.g. "PROBLEM_NUMBER=1")
 def main() -> int:
     parser = argparse.ArgumentParser()
     try:
-        subparsers = parser.add_subparsers()
+        subparsers = parser.add_subparsers(dest='problem_parser')
 
         parser_p1 = subparsers.add_parser('p1')
         parser_p1.add_argument('--program_exec', type=str, required=True)
@@ -20,6 +25,10 @@ def main() -> int:
         parser_p2.set_defaults(func=p2.p2_runner)
 
         args = parser.parse_args()
+
+        problem_num = args.problem_parser.removeprefix("p")
+        os.environ.setdefault(PROBLEM_NUMBER_ENV, problem_num)
+
         args.func(args)
     except argparse.ArgumentTypeError as e:
         parser.print_help()
